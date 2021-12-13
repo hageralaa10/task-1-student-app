21da36776c8a810fc06d0c9fc5b89287374a2fc1
hageralaa10 <48799632+hageralaa10@users.noreply.github.com>
2021-12-13 21:13:38 +0200

    Create student.js

---
+++ student.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+const addStudent = (id, name, grades, comments, total) => {
+    const students = loadStudent();
+    const duplicateID  = students.filter(student => {
+        return id === student.id
+    })
+    if (duplicateID.length === 0) {
+        students.push({id, name, grades, comments, total});
+        saveStudent(students);
+        console.log("added Successfully");
+    } else {
+        console.log("Error duplicated ID");
+    }
+
+};
+
+const loadStudent = () => {
+    try{
+        const studentJSON = fs.readFileSync('student.json');
+        return JSON.parse(studentJSON); 
+    }catch (e) {
+        return [];
+    }
+};
+
+const saveStudent = (student) => {
+    const studentJSON = JSON.stringify(student)
+    fs.writeFileSync('student.json', studentJSON)
+};
+
+
+// Read with id
+const readStudent = (studentId) => {
+    const students = loadStudent();
+    const student = students.find(student => {
+        return student.id === studentId;
+    })
+    
+    console.log(`Student ID: ${student.id}, Student Name: ${student.name}, Student Grade: ${student.grades}, Student Comment: ${student.comments}, Student Total: ${student.total}`);
+    
+};
+
+// Delete
+const removeStudent = (studentId) => {
+    const students = loadStudent();
+    const student = students.filter( student => {
+        return student.id !== studentId;
+    })
+    if (students > student) {
+        saveStudent(student);
+        console.log('deleted successfully');
+    } else {
+        console.log('Please enter valid ID');
+    }
+};
+
+
+// List
+const listStudent = () => {
+    const students = loadStudent();
+    students.forEach(student => {
+        console.log(`Student Name: ${student.name}, Student Total: ${student.total}`);
+    
+    });
+};
+
+module.exports = {
+    addStudent,
+    readStudent,
+    listStudent,
+    removeStudent,
+}
